c08e2a6c209353a8d910d44b8a0a4d4145e45627
Julius Alexander IV <alexaj5rpi@gmail.com>
2016-02-29 20:22:24 -0500

    Example of GridView with text.

--- app/src/main/java/com/temp/temp/MainActivity.java
+++ app/src/main/java/com/temp/temp/MainActivity.java
@@ -30,79 +30,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         GridView gridview = (GridView)findViewById(R.id.gridview);
-        gridview.setAdapter(new SquareViewAdapter(MainActivity.this));
+        gridview.setAdapter(new SquareAdapter(this));
 
-    }
-
-    private class SquareViewAdapter extends BaseAdapter
-    {
-        private List<Item> items = new ArrayList<Item>();
-        private LayoutInflater inflater;
-
-        public SquareViewAdapter(Context context)
-        {
-            inflater = LayoutInflater.from(context);
-
-            items.add(new Item("Image 1", R.drawable.image_1));
-            items.add(new Item("Image 2", R.drawable.image_2));
-            items.add(new Item("Image 3", R.drawable.image_3));
-            items.add(new Item("Image 4", R.drawable.image_4));
-            items.add(new Item("Image 5", R.drawable.image_5));
-        }
-
-        @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        @Override
-        public Object getItem(int i)
-        {
-            return items.get(i);
-        }
-
-        @Override
-        public long getItemId(int i)
-        {
-            return items.get(i).drawableId;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup)
-        {
-            View v = view;
-            ImageView picture;
-            TextView name;
-
-            if(v == null)
-            {
-                v = inflater.inflate(R.layout.grid_item, viewGroup, false);
-                v.setTag(R.id.picture, v.findViewById(R.id.picture));
-                v.setTag(R.id.text, v.findViewById(R.id.text));
-            }
-
-            picture = (ImageView)v.getTag(R.id.picture);
-            name = (TextView)v.getTag(R.id.text);
-
-            Item item = (Item)getItem(i);
-
-            picture.setImageResource(item.drawableId);
-            name.setText(item.name);
-
-            return v;
-        }
-
-        private class Item
-        {
-            final String name;
-            final int drawableId;
-
-            Item(String name, int drawableId)
-            {
-                this.name = name;
-                this.drawableId = drawableId;
-            }
-        }
     }
 }
 
--- app/src/main/java/com/temp/temp/SquareAdapter.java
+++ app/src/main/java/com/temp/temp/SquareAdapter.java
@@ -2,16 +2,21 @@ package com.temp.temp;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by whites5 on 2/24/16.
  */
 public class SquareAdapter extends  BaseAdapter{
     private Context mContext;
+    private final String[] vals = new String[] {"Spencer", "Julius", "Peter", "Ray", "Spetelius"};
 
     public SquareAdapter(Context c) {
         mContext = c;
@@ -19,8 +24,7 @@ public class SquareAdapter extends  BaseAdapter{
 
     @Override
     public int getCount() {
-
-        return 10;
+        return vals.length;
     }
 
     @Override
@@ -36,12 +40,26 @@ public class SquareAdapter extends  BaseAdapter{
     }
 
     @Override
-    public ImageView getView(int position, View convertView, ViewGroup parent) {
-        ImageView view;
-        view = new ImageView(mContext);
-        view.setBackgroundColor(Color.rgb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
-        view.setImageResource(position);
-        return view;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View gv;
+        if (convertView == null) {
+            // get layout from mobile.xml
+            gv = inflater.inflate(R.layout.grid_item, null);
+
+            // set value into textview
+            TextView textView = (TextView) gv.findViewById(R.id.grid_item_text);
+            textView.setText(vals[position]);
+
+            Random rnd = new Random();
+            int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            gv.setBackgroundColor(color);
+        } else {
+            gv = convertView;
+        }
+
+        return gv;
     }
 
 }
