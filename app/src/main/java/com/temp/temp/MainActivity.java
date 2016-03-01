f395ce5efaff9a4e1ed78703537eb3fee3b4b223
Julius Alexander IV <alexaj5rpi@gmail.com>
2016-02-29 22:24:38 -0500

    Shows an example implementation of a GridView with randomly colored squares.

--- app/src/main/java/com/temp/temp/MainActivity.java
+++ app/src/main/java/com/temp/temp/MainActivity.java
@@ -7,11 +7,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
@@ -32,6 +35,20 @@ public class MainActivity extends AppCompatActivity {
         GridView gridview = (GridView)findViewById(R.id.gridview);
         gridview.setAdapter(new SquareAdapter(this));
 
+//        int ideal_size = calculateIdealSize();
+//        ViewGroup.LayoutParams params = gridview.getLayoutParams();
+//        params.width = ideal_size;
+//        params.height = ideal_size;
+//        gridview.setLayoutParams(params);
+//        Log.d("Size", Integer.toString(ideal_size));
+    }
+
+    private int calculateIdealSize() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int boxes = Math.round(width / 185);
+        return width/boxes;
     }
 }
 
--- app/src/main/java/com/temp/temp/SquareAdapter.java
+++ app/src/main/java/com/temp/temp/SquareAdapter.java
@@ -2,11 +2,14 @@ package com.temp.temp;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -39,6 +42,21 @@ public class SquareAdapter extends  BaseAdapter{
         return 0;
     }
 
+    private int calculateIdealSize() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(metrics);
+        // in dp
+        double idealSize = 182.5;
+        float density = mContext.getResources().getDisplayMetrics().density;
+        int screenWidth = (int) Math.round(metrics.widthPixels/density);
+        int numSquares = (int) Math.round(screenWidth/idealSize);
+        Log.d("Screen Width", Integer.toString(screenWidth));
+        Log.d("Ideal Width", Integer.toString(screenWidth/numSquares));
+
+        return (int) (screenWidth*density)/numSquares;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) mContext
@@ -46,7 +64,7 @@ public class SquareAdapter extends  BaseAdapter{
         View gv;
         if (convertView == null) {
             // get layout from mobile.xml
-            gv = inflater.inflate(R.layout.grid_item, null);
+            gv = inflater.inflate(R.layout.grid_item_container, null);
 
             // set value into textview
             TextView textView = (TextView) gv.findViewById(R.id.grid_item_text);
@@ -55,6 +73,9 @@ public class SquareAdapter extends  BaseAdapter{
             Random rnd = new Random();
             int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
             gv.setBackgroundColor(color);
+
+            int ideal_size = calculateIdealSize();
+            gv.setLayoutParams(new GridView.LayoutParams(ideal_size, ideal_size));
         } else {
             gv = convertView;
         }
